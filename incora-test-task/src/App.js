e2205a0bdcfe1e498005c5ff9e740d0c8ec76904
Marian Huliak <marian.guliak@gmail.com>
2024-06-04 02:10:33 +0300

    done

--- incora-test-task/src/App.js
+++ incora-test-task/src/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Users from './components/Users';
 import Posts from './components/Posts';
+import './App.css'
 
 function App() {
   return (
     <>
-    <h1>Hello</h1>
     <Router>
       <div className="App">
-        <header className="App-header">
-          <h1>Incora Test Task</h1>
-        </header>
         <main>
           <Routes>
             <Route path="/" element={<Users />} />
