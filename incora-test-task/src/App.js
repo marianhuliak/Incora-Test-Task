37411aca418b00ac9216a134cee7ff7856a53fab
Marian Huliak <marian.guliak@gmail.com>
2024-06-04 17:27:50 +0300

    done

--- incora-test-task/src/App.js
+++ incora-test-task/src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Users from './components/Users';
-import Posts from './components/Posts';
-import PostDetails from './components/PostDetails';
-import AddPost from './components/AddPost';
-import EditPost from './components/EditPost';
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Users from "./components/Users";
+import Posts from "./components/Posts";
+import PostDetails from "./components/PostDetails";
+import AddPost from "./components/AddPost";
+import EditPost from "./components/EditPost";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
-        <main>
-          <Routes>
-            <Route path="/" element={<Users />} />
-            <Route path="/posts/:userId" element={<Posts />} />
-            <Route path="/posts/details/:postId" element={<PostDetails />} />
-            <Route path="/posts/:userId/add" element={<AddPost />} />
-            <Route path="/posts/edit/:postId" element={<EditPost />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/posts/:userId" element={<Posts />} />
+          <Route path="/posts/details/:postId" element={<PostDetails />} />
+          <Route path="/posts/:userId/add" element={<AddPost />} />
+          <Route path="/posts/edit/:postId" element={<EditPost />} />
+        </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
